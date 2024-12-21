7458c04572626a83d430d3a283682b0b7b228541
Adwait Relekar <adrelekar23@gmail.com>
2024-12-21 16:22:04 -0500

    Exercise 1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,59 @@
-class Stack { 
+// Time Complexity: O(1) for all the methods
+// Space Complexity: O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
+    static final int MAX = 1000;
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Write your code here
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top == a.length-1) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        a[++top] = x;
+        return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top == -1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int res = a[top--];
+        return res;
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(top == -1) {
+            System.out.println("Stack is empty");
+            return 0;
+        }
+        return a[top];
     } 
 } 
   
@@ -37,10 +61,10 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
         System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
